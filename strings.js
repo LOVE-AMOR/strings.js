b0a6a7558e7384e698328e13b584f798761ef631
LOVE-AMOR <loveamorbalaqui2@gmail.com>
2024-09-13 21:05:20 -0800

    Iniial commit

---
+++ strings.js
@@ -0,0 +1,17 @@
+let favActorFirstName = 'Eddie';
+console.log('favActorFirstName:', favActorFirstName);
+
+let favActorLastName = 'Garcia';
+console.log('favActorLastName:', favActorLastName);
+
+let fullName = favActorFirstName + ' ' + favActorLastName;
+console.log('fullName:', fullName);
+
+let uppercase = favActorFirstName.toUpperCase();
+console.log('uppercase:', uppercase);
+
+let lowercase = favActorLastName.toLowerCase();
+console.log('lowercase:', lowercase);
+
+let message = 'My favorite actor is ' + fullName.toUpperCase() + '. His best show is Silicon Valley';
+console.log('message:', message);
